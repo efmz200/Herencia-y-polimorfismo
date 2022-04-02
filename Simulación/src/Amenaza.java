495d779c9393eb8019f78b4d96a4cca2c9809ace
Erick <efmz2000@outlook.es>
2022-04-01 18:26:09 -0600

    correcion del revisar visda de la amenaza

--- Simulación/src/Amenaza.java
+++ Simulación/src/Amenaza.java
@@ -5,7 +5,7 @@ public class Amenaza extends Objeto {
 		super(vida, pos_x, pos_y);
 	}
 	public void revisarVida(){
-		if (this.get_vida()== -1){
+		if (this.get_vida()>-1){
 			set_pos_x((int) Math.random() *49);
 			set_pos_y((int) Math.random() *49);
 			set_vida(10);
