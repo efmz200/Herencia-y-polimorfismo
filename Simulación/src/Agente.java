9a207b8704c6ffa947f357a975d0ecfd9a107112
Erick <efmz2000@outlook.es>
2022-04-03 19:41:42 -0600

    se elimina la variable de movimiento

--- Simulación/src/Agente.java
+++ Simulación/src/Agente.java
@@ -8,44 +8,31 @@ public abstract class Agente {
     private int pos_y_objetivo;
     private int pos_x_objetivo;
     private int estado;//0 si esta neutro, 1 si esta atacando, -1 si está huyendo
-    private boolean lleva_carga;
-    private boolean se_movio;
+    private boolean lleva_carga;    
     
     public Agente(int y,int x){
         pos_y=y;
         pos_x=x;
         estado=0;
-        lleva_carga=false;
-        se_movio=false;
-        
-                 
+        lleva_carga=false;   
     }
     public abstract void recolectar();//se debe cambiar para que reciva una lista de objetos
     public abstract boolean buscar_aliado();//
     public abstract boolean buscar_amenaza();
     public abstract void marcar_objetivo();
-    public abstract void buscar_recurso();
+    public abstract void buscar_recurso();    
+    public abstract void buscar_amenaza(List<Objeto> objetos);    
 
-    
-    public void buscar_amenaza(List<Object> objetos){
-        for(int i=0;i<objetos.size();i++){
-            Object obj_aux=objetos.get(i);
-            if(obj_aux.)
-        }
 
-    }
     public void buscar_aliado(List<Agente> aliados){   
         for(int i=0;i<aliados.size();i++){
             Agente agt_aux= aliados.get(i);
             if(Math.abs(agt_aux.pos_y-pos_y)==1 && Math.abs(agt_aux.pos_x-pos_x)==1){//Revisa que el aliado este en un radio de 1
 
             }
-        }    
-
+        } 
     }
     public boolean colision(Agente agt){
-
-
         return true;
     }
     public void mover(int direccion){//se debe cambiar para que reciva una lista de objetos para saber si hay obstaculos
@@ -55,19 +42,15 @@ public abstract class Agente {
         switch(direccion){
             case 1:
                 pos_y++;
-                se_movio=true;
                 break;
             case 2:
                 pos_y--;
-                se_movio=true;
                 break;
             case 3:
                 pos_x++;
-                se_movio=true;
                 break;
             case 4:
                 pos_x--;
-                se_movio=true;
                 break;
         }
     } 
@@ -90,10 +73,7 @@ public abstract class Agente {
     }
     public void set_carga(boolean carga){
         lleva_carga=carga;
-    }
-    public void set_se_movio(boolean movio){
-        se_movio=movio;
-    }  
+    }     
 
     public int get_pos_y(){
         return pos_y;
