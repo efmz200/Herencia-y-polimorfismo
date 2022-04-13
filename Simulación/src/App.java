0876e31984be058999e96f8b1bf6f3e1803dae77
Drump1 <efmz2000@outlook.es>
2022-04-13 11:07:13 -0600

    cambios en el main para hacer pruebas

--- Simulación/src/App.java
+++ Simulación/src/App.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hi");
-        System.out.println("holi, World!");
+        Simulacion simulacion= new Simulacion();
+        simulacion.iniciar();
+        simulacion.obtener_matriz();        
+        simulacion.simular();
+        //simulacion.obtener_matriz();
+        ArrayList<ArrayList<Integer>> matriz=simulacion.obtener_matriz();
+        for (int i=0; i<matriz.size();i++){
+            System.out.println(matriz.get(i));
+        }
     }
 }
