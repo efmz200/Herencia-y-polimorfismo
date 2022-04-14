3cadf50b40fc796fd0df4e333df557ffbc610da7
Drump1 <efmz2000@outlook.es>
2022-04-14 11:08:20 -0600

    arreglos en la funcion de ataque de amanezas

--- Simulación/src/Defensor.java
+++ Simulación/src/Defensor.java
@@ -14,23 +14,35 @@ public class Defensor extends Agente{
         int amenaza_x=amemanaza.get_pos_x();
         int amenaza_y=amemanaza.get_pos_y();
         if(Math.abs(x-amenaza_x)==1 && Math.abs(y-amenaza_y)==1){
-            amemanaza.set_vida(amemanaza.get_vida()-1);
+            amemanaza.set_vida(amemanaza.get_vida()+1);
             objamenazas.set(num_amenaza,amemanaza);
         }
-        switch ((int) Math.random()*1){// decide si acercarse al aliado en x o en y
+        switch ((int) Math.random()*1){// decide si acercarse a la amenaza en x o en y
             case 0:// se acerca en x
                 if(x<amenaza_x){
-                    mover(3,objamenazas);    
+                    if(get_pos_x()==0){
+                        break;
+                    }
+                    set_pos_x(get_pos_x()+1);   
                 }
-                if(x>amenaza_x){
-                    mover(4,objamenazas);    
+                if(x>amenaza_x+1){
+                    if(get_pos_x()==49){
+                        break;
+                    }
+                    set_pos_x(get_pos_x()-1);                       
                 }
             case 1:// se acerca en y
                 if(y<amenaza_y){
-                    mover(1,objamenazas);    
+                    if(get_pos_y()==0){
+                        break;
+                    }
+                    set_pos_y(get_pos_y()-1);    
                 }
-                if(y>amenaza_y){
-                    mover(2,objamenazas);    
+                if(y>amenaza_y+1){
+                    if(get_pos_y()==49){
+                        break;
+                    }
+                    set_pos_y(get_pos_y()+1);    
                 }                                
         }
     }
