0d6a1ffd12954ddb04125e57320a385dbabb74b8
Drump1 <efmz2000@outlook.es>
2022-04-14 10:00:45 -0600

    se eliminarion las lineas para pruebas del main

--- Simulación/src/App.java
+++ Simulación/src/App.java
@@ -2,15 +2,6 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hi");
-        Simulacion simulacion= new Simulacion();
-        simulacion.iniciar();
-        simulacion.obtener_matriz();        
-        simulacion.simular();
-        //simulacion.obtener_matriz();
-        ArrayList<ArrayList<Integer>> matriz=simulacion.obtener_matriz();
-        for (int i=0; i<matriz.size();i++){
-            System.out.println(matriz.get(i));
-        }
+        Ventana vent= new Ventana();
     }
 }
