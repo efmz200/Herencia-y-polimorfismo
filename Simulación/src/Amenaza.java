4ace5559842546de7361d93b79cb634838700a51
Drump1 <efmz2000@outlook.es>
2022-04-14 11:07:10 -0600

    cambios en el modificador de vida de la amenaza y recurso

--- Simulación/src/Amenaza.java
+++ Simulación/src/Amenaza.java
@@ -5,7 +5,9 @@ public class Amenaza extends Objeto {
 		super(vida, pos_x, pos_y);
 	}
 	public void revisarVida(){
-		if (this.get_vida()>-1){
+		System.out.println("Me quedan: "+get_vida()+ " puntos de vida");
+		if (this.get_vida()==-1){
+			System.out.println("Se eliminó una amenaza");
 			set_pos_x((int) Math.random() *46 +2);
 			set_pos_y((int) Math.random() *46 +2);
 			set_vida(-11);
